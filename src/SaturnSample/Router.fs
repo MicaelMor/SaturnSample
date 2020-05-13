f62894f1e77d6ecb5263089d4fa38509fe5209dc
Micael Morais <micaelmor@gmail.com>
2020-05-13 16:43:28 +0100

    Replaced error_handler with not_found_handler
    Also removed the extra line in the comments to make the code more usable after removing the comments.

--- src/SaturnSample/Router.fs
+++ src/SaturnSample/Router.fs
@@ -28,19 +28,19 @@ let browserRouter = router {
 
 //Other scopes may use different pipelines and error handlers
 
-// let api = pipeline {
-//     plug acceptJson
-//     set_header "x-pipeline-type" "Api"
-// }
+//let api = pipeline {
+//    plug acceptJson
+//    set_header "x-pipeline-type" "Api"
+//}
 
-// let apiRouter = router {
-//     error_handler (text "Api 404")
-//     pipe_through api
-//
-//     forward "/someApi" someScopeOrController
-// }
+//let apiRouter = router {
+//    not_found_handler (text "Api 404")
+//    pipe_through api
+
+//    forward "/someApi" someScopeOrController
+//}
 
 let appRouter = router {
-    // forward "/api" apiRouter
+    //forward "/api" apiRouter
     forward "" browserRouter
 }
